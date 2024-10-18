a2bf0a2edac12c9310df058050f1ad29aec4e8ad
Andrei Labadaev <andrei-labadaev@mail.ru>
2024-10-18 16:46:04 +0300

    uppdate

---
+++ src/assets/js/app.js
@@ -0,0 +1,7 @@
+//import * as isWepb from "./vendor/isWebp.js";
+//isWepb.isWebp();
+--include('vendor/isWebp.js') 
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Код, который будет выполнен после загрузки и инициализации DOM
+});
