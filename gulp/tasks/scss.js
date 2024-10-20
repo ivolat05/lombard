2dbfa3662f5ff73f83adf9624a1922db664b0272
Andrei Labadaev <andrei-labadaev@mail.ru>
2024-10-20 19:43:26 +0300

    fix gulp

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -10,44 +10,46 @@ import groupCssMediaQueries from "gulp-group-css-media-queries"; // Группи
 const sass = gulpSass(dartSass);
 
 export const scss = () => {
-	return app.gulp
-		.src(app.path.src.scss, { sourcemaps: app.isDev })
-		.pipe(
-			app.plugins.plumber(
-				app.plugins.notify.onError({
-					title: "SCSS",
-					message: "Error: <%= error.message %>",
+	return (
+		app.gulp
+			.src(app.path.src.scss, { sourcemaps: app.isDev })
+			.pipe(
+				app.plugins.plumber(
+					app.plugins.notify.onError({
+						title: "SCSS",
+						message: "Error: <%= error.message %>",
+					})
+				)
+			)
+			.pipe(app.plugins.replace(/@img\//g, "../img/"))
+			.pipe(
+				sass({
+					outputStyle: "expanded",
 				})
 			)
-		)
-		.pipe(app.plugins.replace(/@img\//g, "../img/"))
-		.pipe(
-			sass({
-				outputStyle: "expanded",
-			})
-		)
-		.pipe(app.plugins.if(app.isBuild, groupCssMediaQueries()))
-		.pipe(
-			app.plugins.if(
-				app.isBuild,
-				webpcss({
-					webpClass: ".webp",
-					noWebpClass: ".no-webp",
+			.pipe(app.plugins.if(app.isBuild, groupCssMediaQueries()))
+			// .pipe(
+			// 	app.plugins.if(
+			// 		app.isBuild,
+			// 		webpcss({
+			// 			webpClass: ".webp",
+			// 			noWebpClass: ".no-webp",
+			// 		})
+			// 	)
+			// )
+			.pipe(
+				autoprefixer({
+					grid: true,
+					overrideBrowserslist: ["last 8 versions"],
+					cascade: true,
 				})
 			)
-		)
-		.pipe(
-			autoprefixer({
-				grid: true,
-				overrideBrowserslist: ["last 8 versions"],
-				cascade: true,
-			})
-		)
 
-		.pipe(app.gulp.dest(app.path.build.css)) // Если нужен только сжатый закаментирова
-		.pipe(app.plugins.if(app.isBuild, cleanCss()))
-		.pipe(rename({ extname: ".min.css" }))
-		.pipe(app.gulp.dest(app.path.build.css))
+			.pipe(app.gulp.dest(app.path.build.css)) // Если нужен только сжатый закаментирова
+			.pipe(app.plugins.if(app.isBuild, cleanCss()))
+			.pipe(rename({ extname: ".min.css" }))
+			.pipe(app.gulp.dest(app.path.build.css))
 
-		.pipe(app.plugins.browsersync.stream());
+			.pipe(app.plugins.browsersync.stream())
+	);
 };
